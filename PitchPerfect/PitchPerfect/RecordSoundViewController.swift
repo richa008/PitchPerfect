5060170854ab17e4bf6eb2975c9707a89ca1ea00
Deshmukh,Richa <Richa.Deshmukh@Cerner.com>
2016-04-17 20:11:35 -0500

    Adding icons and structure

---
+++ PitchPerfect/PitchPerfect/RecordSoundViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Deshmukh,Richa on 4/17/16.
+//  Copyright © 2016 Richa. All rights reserved.
+//
+
+import UIKit
+
+class RecordSoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PitchPerfect/PlaySoundViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PlaySoundViewController.swift
+//  PitchPerfect
+//
+//  Created by Deshmukh,Richa on 4/17/16.
+//  Copyright © 2016 Richa. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundViewController: UIViewController {
+
+}
